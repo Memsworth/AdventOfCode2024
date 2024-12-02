d6559402fed329bb5dd95aa6628afd0f8d6cceaf
Abubakar Ahmed <abubakarmahfooz4479@outlook.com>
2024-12-02 22:23:25 +0530

    Done with Day 01

---
+++ AoC2024/Day01.cs
@@ -0,0 +1,64 @@
+﻿using AoCHelper;
+
+namespace AoC2024
+{
+    public class Day01 : BaseDay
+    {
+        private readonly string[] _input;
+
+        public Day01()
+        {
+            _input = File.ReadAllLines(InputFilePath);
+        }
+        public override ValueTask<string> Solve_1() =>
+            new($"{SolvingOne().ToString()}");
+
+        public override ValueTask<string> Solve_2() =>
+            new($"{SolvingTwo()}");
+
+        private int SolvingOne()
+        {
+            var leftList = new List<int>();
+            var rightList = new List<int>();
+
+            foreach (var line in _input)
+            {
+                var item = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                leftList.Add(int.Parse(item[0]));
+                rightList.Add(int.Parse(item[1]));
+            }
+            
+            leftList.Sort();
+            rightList.Sort();
+            
+            var result = leftList.Zip(rightList, (l, r) => Math.Abs(l - r)).ToList();
+            var total = result.Sum();
+            return total;
+        }
+
+        private int SolvingTwo()
+        {
+            var leftList = new List<int>();
+            var rightList = new List<int>();
+
+            foreach (var line in _input)
+            {
+                var item = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                leftList.Add(int.Parse(item[0]));
+                rightList.Add(int.Parse(item[1]));
+            }
+
+            var noDuplicates = leftList.Distinct().ToList();
+            var dictionary = noDuplicates.ToDictionary(k => k, k => 0);
+
+            foreach (var number in rightList.Where(number => dictionary.ContainsKey(number)))
+            {
+                dictionary[number]++;
+            }
+
+            var total = leftList.Sum(number => (number * dictionary[number]));
+            
+            return total;
+        }
+    }
+}
--- AoC2024/Program.cs
+++ AoC2024/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AoC2024;
+using AoCHelper;
+
+await Solver.SolveAll();
